4eaa3fa5eff3f2eeebf6ab7fc1b6a737756b4f1a
James Stevens <jtg.stevens@outlook.com>
2024-06-20 18:11:50 +0100

    Run black

--- sonos_inkyphat/sonos_inkyphat.py
+++ sonos_inkyphat/sonos_inkyphat.py
@@ -3,22 +3,38 @@ import asyncio
 import logging
 import argparse
 
+
 def callback(state):
     logging.debug("In callback")
     logging.info(state.track)
+
 
 async def main():
     parser = argparse.ArgumentParser()
     parser.add_argument("room", help="ID of the Sonos room to watch")
     parser.add_argument("endpoint", help="IP or DNS name of Home Assistant server")
     parser.add_argument("access_token", help="Home Assistant access token")
-    parser.add_argument("-d", "--debug", action="store_const", dest="loglevel", const=logging.DEBUG, default=logging.INFO, help="enable debug logging")
+    parser.add_argument(
+        "-d",
+        "--debug",
+        action="store_const",
+        dest="loglevel",
+        const=logging.DEBUG,
+        default=logging.INFO,
+        help="enable debug logging",
+    )
     args = parser.parse_args()
 
-    logging.basicConfig(format="%(asctime)s - %(name)20s - %(funcName)30s - %(levelname)5s - %(message)s", level=args.loglevel)
+    logging.basicConfig(
+        format="%(asctime)s - %(name)20s - %(funcName)30s - %(levelname)5s - %(message)s",
+        level=args.loglevel,
+    )
 
     logging.info(f"Subscribing to {args.room} at {args.endpoint}")
-    sub = await sonos_ws.SonosSubscription.create(args.room, args.endpoint, args.access_token, callback)
+    sub = await sonos_ws.SonosSubscription.create(
+        args.room, args.endpoint, args.access_token, callback
+    )
+
 
 if __name__ == "__main__":
     asyncio.run(main())
--- sonos_ws/sonos_ws/__init__.py
+++ sonos_ws/sonos_ws/__init__.py
@@ -8,27 +8,30 @@ logger = logging.getLogger(__name__)
 handler = logging.NullHandler()
 logger.addHandler(handler)
 
+
 def to_ws_string(object):
     """Helper function to convert a Python dict to an object to pass to the HA Websockets API. For some reason, it must use double quotes."""
-    return str(object).replace("'", "\"")
+    return str(object).replace("'", '"')
 
 
 class SonosRoomState:
     def __init__(self, ws_new_state):
         """Create a SonosRoomState from a 'new_state' WS object."""
         try:
-            self.id = ws_new_state['entity_id'][13:] # strip off 'media_player.' prefix
-            self.name = ws_new_state['attributes']['friendly_name']
-            self.state = ws_new_state['state']
+            self.id = ws_new_state["entity_id"][13:]  # strip off 'media_player.' prefix
+            self.name = ws_new_state["attributes"]["friendly_name"]
+            self.state = ws_new_state["state"]
 
-            self.track = ws_new_state['attributes'].get('media_title')
-            self.artist = ws_new_state['attributes'].get('media_artist')
-            self.album = ws_new_state['attributes'].get('media_album_name')
-            self.playlist = ws_new_state['attributes'].get('media_playlist')
-            self.muted = ws_new_state['attributes'].get('is_volume_muted')
-            self.shuffle = ws_new_state['attributes'].get('shuffle')
+            self.track = ws_new_state["attributes"].get("media_title")
+            self.artist = ws_new_state["attributes"].get("media_artist")
+            self.album = ws_new_state["attributes"].get("media_album_name")
+            self.playlist = ws_new_state["attributes"].get("media_playlist")
+            self.muted = ws_new_state["attributes"].get("is_volume_muted")
+            self.shuffle = ws_new_state["attributes"].get("shuffle")
         except:
-            logger.error(f"Error creating SonosRoomState for this input: {ws_new_state}")
+            logger.error(
+                f"Error creating SonosRoomState for this input: {ws_new_state}"
+            )
             pass
 
     def __str__(self):
@@ -36,15 +39,12 @@ class SonosRoomState:
 
 
 async def connect(sock, access_token):
-    auth_obj = {
-        "type": "auth",
-        "access_token": access_token
-    }
+    auth_obj = {"type": "auth", "access_token": access_token}
 
     authreqd_message = await sock.recv()
     obj = json.loads(authreqd_message)
     logger.debug(f"Expecting auth_required, received {obj}")
-    assert(obj['type'] == "auth_required")
+    assert obj["type"] == "auth_required"
 
     logger.debug(f"Sending auth message: {auth_obj}")
     await sock.send(to_ws_string(auth_obj))
@@ -52,7 +52,7 @@ async def connect(sock, access_token):
     second_message = await sock.recv()
     obj = json.loads(second_message)
     logger.debug(f"Expecting auth_ok, received {obj}")
-    assert(obj['type'] == "auth_ok")
+    assert obj["type"] == "auth_ok"
 
 
 async def subscribe(sock):
@@ -64,7 +64,7 @@ async def subscribe(sock):
     await sock.send(to_ws_string(event_sub))
     rsp = await sock.recv()
     obj = json.loads(rsp)
-    assert(obj['type'] == "result" and obj['success'])
+    assert obj["type"] == "result" and obj["success"]
 
 
 class SonosSubscription:
@@ -86,8 +86,8 @@ class SonosSubscription:
             while True:
                 msg = await sock.recv()
                 obj = json.loads(msg)
-                if obj['event']['data']['entity_id'].startswith('media_player'):
-                    state = SonosRoomState(obj['event']['data']['new_state'])
+                if obj["event"]["data"]["entity_id"].startswith("media_player"):
+                    state = SonosRoomState(obj["event"]["data"]["new_state"])
                     logger.debug(f"New state: {state}")
                     if state.id == self.id:
                         logger.debug("Matched ID, calling callback")
